eadfcaf285b305fe063ce11a9830a4f2bd8bc9c0
ShreyaDas2810 <das.shr3ya@gmail.com>
2025-04-26 18:39:18 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,11 +4,16 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        
+        // Print a welcome message to the console
         System.out.printf("Hello and welcome!");
 
+        // Loop through numbers from 1 to 5
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            
+            // Print the current value of 'i' during each iteration of the loop
             System.out.println("i = " + i);
         }
     }
