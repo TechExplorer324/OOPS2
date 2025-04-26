662e21c6a3d552f4ec39c380915da2c1b7e6e689
ShreyaDas2810 <das.shr3ya@gmail.com>
2025-04-26 18:29:24 +0530

    Update Bike.java

--- Bike.java
+++ Bike.java
@@ -1,11 +1,25 @@
 /**
- * Represents a Bike/Motorcycle.
+ * Represents a Bike or Motorcycle in the parking system.
+ * Inherits from the Vehicle class and specifies behavior unique to bikes.
  */
 class Bike extends Vehicle {
+
+    /**
+     * Constructor to create a Bike object.
+     *
+     * @param licensePlate The license plate number of the bike.
+     * @param owner The owner of the bike (a User object).
+     */
     public Bike(String licensePlate, User owner) {
+        // Initialize the Bike by calling the parent (Vehicle) constructor
         super(licensePlate, VehicleType.BIKE, owner);
     }
 
+    /**
+     * Specifies the type of parking spot required for the bike.
+     *
+     * @return SpotType.MOTORBIKE indicating that a motorbike spot is needed.
+     */
     @Override
     public SpotType getRequiredSpotType() {
         return SpotType.MOTORBIKE;
