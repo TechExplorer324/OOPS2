c3c88ca1c738fa278f9439361de24c7054aaee42
ShreyaDas2810 <das.shr3ya@gmail.com>
2025-04-26 19:01:59 +0530

    Update VehicleType.java

--- VehicleType.java
+++ VehicleType.java
@@ -1,6 +1,11 @@
 /**
  * Enumeration for different types of vehicles.
+ * This enum defines various vehicle categories that can be used within the system.
+ * Each constant represents a specific type of vehicle.
  */
 enum VehicleType {
-    CAR, BIKE, TRUCK, ELECTRIC_VEHICLE
+    CAR,              // Represents a standard car (e.g., sedan, hatchback)
+    BIKE,             // Represents a motorcycle or scooter
+    TRUCK,            // Represents a large vehicle, typically for transporting goods
+    ELECTRIC_VEHICLE  // Represents a vehicle powered by electricity (e.g., electric car, e-bike)
 }
