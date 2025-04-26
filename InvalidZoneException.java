2dbaac09245945c810f45aa626b90b59a66945b5
ShreyaDas2810 <das.shr3ya@gmail.com>
2025-04-26 18:26:46 +0530

    Update InvalidZoneException.java

--- InvalidZoneException.java
+++ InvalidZoneException.java
@@ -2,7 +2,23 @@
  * Custom Exception (XI) for when an invalid or non-existent zone is specified.
  */
 class InvalidZoneException extends Exception {
+
+    // Default error message
+    private static final String DEFAULT_MESSAGE = "Specified zone is invalid or does not exist.";
+
+    /**
+     * Constructor with custom message.
+     *
+     * @param message Custom error message.
+     */
     public InvalidZoneException(String message) {
         super(message);
+    }
+
+    /**
+     * Constructor with default message.
+     */
+    public InvalidZoneException() {
+        super(DEFAULT_MESSAGE);
     }
 }
