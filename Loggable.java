48be1bcdae5a4978b5a76aa6b2dbec023cd22063
ShreyaDas2810 <das.shr3ya@gmail.com>
2025-04-26 18:36:52 +0530

    Update Loggable.java

--- Loggable.java
+++ Loggable.java
@@ -1,11 +1,16 @@
 /**
  * Interface (VI, VIII) for objects that can log messages.
- * Used to demonstrate multiple interface implementation.
+ * This interface is used to demonstrate the concept of multiple interface implementation.
+ * Classes implementing this interface must provide a concrete implementation for logging messages.
  */
 interface Loggable {
     /**
      * Logs a message.
+     * 
      * @param message The message to log.
+     * This method is intended to be implemented by classes that need to log specific messages
+     * to a logging system, file, or any other medium.
      */
     void log(String message);
 }
+
