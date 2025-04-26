a0b90c9a059f96cf8d9caa42230045ab890f54f9
ShreyaDas2810 <das.shr3ya@gmail.com>
2025-04-26 18:18:28 +0530

    Update Bike.java

--- Bike.java
+++ Bike.java
@@ -11,3 +11,31 @@ class Bike extends Vehicle {
         return SpotType.MOTORBIKE;
     }
 }
+/**
+ * Represents a Bike or Motorcycle in the system.
+ * Inherits from the Vehicle class.
+ */
+class Bike extends Vehicle {
+
+    /**
+     * Constructor for Bike.
+     *
+     * @param licensePlate The license plate number of the bike.
+     * @param owner The User who owns the bike.
+     */
+    public Bike(String licensePlate, User owner) {
+        // Call the parent (Vehicle) constructor with license plate, vehicle type, and owner
+        super(licensePlate, VehicleType.BIKE, owner);
+    }
+
+    /**
+     * Returns the type of parking spot required for this vehicle.
+     * Bikes require a MOTORBIKE type spot.
+     *
+     * @return SpotType.MOTORBIKE
+     */
+    @Override
+    public SpotType getRequiredSpotType() {
+        return SpotType.MOTORBIKE;
+    }
+}
